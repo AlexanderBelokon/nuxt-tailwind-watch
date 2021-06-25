2dfeb654ea344b4195a51e6022ad1b393d84b9a7
Alexander <aab.sandash@protonmail.com>
2021-06-25 14:56:32 +0300

    Added purge and removed jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    mode: 'jit',
+    purge: {
+        content: [
+            'components/**/*.vue',
+            'layouts/**/*.vue',
+            'pages/**/*.vue',
+            'templates/**/*.vue',
+            'plugins/**/*.js',
+            'nuxt.config.{js,ts}',
+        ],
+    },
 }
